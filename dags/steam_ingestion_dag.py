98bcc5a96111b50a5af964b47d7044d8bdb9576f
Dulain-Willis <yellowstripes34@gmail.com>
2025-07-12 12:33:21 -0400

    Update steam_ingestion_dag.py 10k ----15k

--- dags/steam_ingestion_dag.py
+++ dags/steam_ingestion_dag.py
@@ -26,7 +26,7 @@ def steam_ingestion():
         url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
         response = requests.get(url)
         response.raise_for_status()
-        return response.json()["applist"]["apps"][5000:10000]  # Limit for dev
+        return response.json()["applist"]["apps"][10000:15000]  # Limit for dev
 
     @task()
     def fetch_game_details(apps):
