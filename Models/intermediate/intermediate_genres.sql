94ac3bff101f9155efaa66d50ba1f799bd10641b
Dulain Willis <dulainslaptop@Dulains-Air.lan>
2025-08-19 14:09:18 -0400

    started categories and updated logic for int models

--- Models/intermediate/intermediate_genres.sql
+++ Models/intermediate/intermediate_genres.sql
@@ -5,31 +5,27 @@
 	)
 }}
 
+--select appid and genres from staging but make null rows blank, remove any spaces around commas, and 
+--split the string into an array using a comma as the delimiter
+
 WITH base AS (
 	SELECT
-		app_id,
-		split(
-			regexp_replace(
-				coalesce(genres, ''),
-			'\\s*,\\s*', ','),
-		',') AS split_genres
+		appid,
+		SPLIT(REGEXP_REPLACE(COALESCE(genres, ''), '\\s*,\\s*', ','), ',') AS split_genres
 	FROM {{ ref('staging_steam_games') }}
 ),
 
-flatten AS (
-	SELECT
-		app_id,
-		NULLIF(TRIM(f.value)::varchar, '') AS genre_raw
-	FROM base,
-		lateral flatten(input => split_genres) AS f
-),
+--select appid and genres from the previous CTE, flattening the split genres array and keeping only the
+--values that are not null and not blank either
 
 final AS (
 	SELECT
-		app_id,
-		NULLIF(genre_raw, '') AS genre
-	FROM flatten
-	WHERE NULLIF(genre_raw, '') IS NOT NULL
+		appid,
+		LOWER(TRIM(f.value))::VARCHAR AS genres
+	FROM base,
+		LATERAL FLATTEN(input => split_genres) AS f
+	WHERE f.value IS NOT NULL
+		AND TRIM(f.value) <> '' 
 )
 
 SELECT * FROM final
