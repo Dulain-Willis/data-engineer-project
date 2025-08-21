218f80d3e4db5f3e4d1f5a40f5c79606451c5717
dulain <yellowstripes34@gmail.com>
2025-08-21 13:41:58 -0400

    started int yml and made int lang sql

---
+++ Models/intermediate/intermediate_languages.sql
@@ -0,0 +1,29 @@
+{{
+	config(
+		alias='int_languages'
+		materialized='view'
+	)
+}}
+
+WITH base AS (
+	SELECT
+        appid,
+        SPLIT(
+            REGEXP_REPLACE(
+                COALESCE(languages, ''),
+            '<.*?>|\\*|languages with full audio support', ''),
+        ',')
+        AS raw_languages
+    FROM {{ ref('staging_steam_games') }}
+),
+
+final AS (
+    SELECT
+        b.appid,
+        LOWER(TRIM(f.value))
+    FROM base AS b,
+        LATERAL FLATTEN(input => raw_languages) AS f
+)
+
+SELECT * FROM final
+	
