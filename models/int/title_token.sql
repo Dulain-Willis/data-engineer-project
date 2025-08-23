b7b6f74fee8db0344884d974638d92408ac5389c
dulain <dulain@DESKTOP-535CNFP.localdomain>
2025-08-23 10:46:42 -0400

    finished title token int model

--- models/int/title_token.sql
+++ models/int/title_token.sql
@@ -5,6 +5,21 @@
 	)
 }}
 
-SELECT
-	TRIM(name)
+WITH base AS (
+    SELECT
+        appid,	
+        SPLIT(REGEXP_REPLACE(TRIM(name), '[^A-Za-z ]', ''), ' ') AS raw_title
+    FROM {{ ref('staging_steam_games') }}
+),
+
+flatten AS (
+    SELECT
+        appid, 
+        f.value::VARCHAR AS words 
+    FROM base,
+        LATERAL FLATTEN(input => raw_title) f
+)
+
+SELECT * FROM flatten
+    
 	
