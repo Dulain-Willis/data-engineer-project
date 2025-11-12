2802071c818e81d9f6b2a30ed1dfcd3a87cb1d4c
dulain <yellowstripes34@gmail.com>
2025-11-12 00:14:57 -0500

    fleshed out ingestion script

--- steamspy_api_ingestion.py
+++ steamspy_api_ingestion.py
@@ -1,3 +1,23 @@
-import requests
+import requests 
 
-response = requests.get("URL GO HERE")
+# 400 is bad 
+# 200 is good
+
+def get_games():
+    url = 'https://steamspy.com/api.php?request=top100in2weeks'
+
+    try:
+        response = requests.get(url)
+        if response.ok:
+            game_details = response
+            print("Successful Request")
+            return game_details
+        else:
+            print("Error:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("Network error:", e)
+
+get_games()
+        
+        
+
