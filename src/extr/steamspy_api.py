0b8799a131042217d5b454b222842d5ea5002d55
dulain <yellowstripes34@gmail.com>
2025-11-14 15:21:39 -0500

    started to integrate terraform

--- src/extr/steamspy_api.py
+++ src/extr/steamspy_api.py
@@ -1,17 +1,19 @@
 import requests
+from requests.exceptions import RequestException
 
 url = "https://steamspy.com/api.php"
+params = {"request": "all"}
 
-def get_steamspy_api:
-   
-    params = {"request": "all"}
+def get_steamspy_api():
 
     try:
         response = requests.get(url, params=params, timeout=30)
         response.raise_for_status()
         print("Successful Request")
         return response.json()
-    except: requests.exception.RequestException as e:
+    except RequestException as e:
         print("Network error:", e)
         return {}
+
+get_steamspy_api()
 
