980d893dedca9b68eca371072c7bbe6567da6d5c
Dulain Willis <dulainslaptop@MacBookAir.lan>
2025-11-22 01:37:44 -0500

    Finished steamspy api call script with pagination

--- src/extr/steamspy_api.py
+++ src/extr/steamspy_api.py
@@ -1,19 +1,41 @@
 import requests
+import time
 from requests.exceptions import RequestException
 
 url = "https://steamspy.com/api.php"
-params = {"request": "all"}
+
+params = {
+    "request": "all",
+    "page": 0
+}
 
 def get_steamspy_api():
+    data_dict = {}
 
-    try:
-        response = requests.get(url, params=params, timeout=30)
-        response.raise_for_status()
-        print("Successful Request")
-        return response.json()
-    except RequestException as e:
-        print("Network error:", e)
-        return {}
+    while True:
 
-get_steamspy_api()
+        try:
+            response = requests.get(url, params=params, timeout=30)
+            response.raise_for_status()
+            print(f"Successful Request for page {params["page"]}")
+            data = response.json()
+
+        except RequestException as e:
+            print("Network error:", e)
+            break
+
+        if not data:
+            break
+
+        data_dict.update(data)
+
+        params["page"] += 1
+
+        print("Sleeping for 60 seconds to respect rate limits")
+        time.sleep(60)
+
+    return data_dict
+
+print(len(get_steamspy_api()))
+
 
