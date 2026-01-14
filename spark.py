9cf44ce60cd78873bc70bcab092885148bbeaff6
Dulain Willis <yellowstripes34@gmail.com>
2026-01-14 16:04:57 -0500

    Wrote Airflow dockerfile to work with spark

--- spark.py
+++ spark.py
@@ -1,9 +1,5 @@
-from pyspark.sql import SparkSession
-
-spark = (
-    SparkSession.builder
-    .appName("sandbox")
-    .getOrCreate()
-)
+import os 
+from pyspark.sql import SparkSession 
+from pyspark.sql import current_timestamp, input_file_name
 
 
